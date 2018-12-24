3eee1b7ba70ec7e9c7c9cc41a2df02aa4fa1ac9d
Michael Fortunato <Michaelfortunato@uchicago.edu>
2018-12-24 00:13:58 -0500

    transition animation v1 created

--- main.js
+++ main.js
@@ -66,24 +66,60 @@ function charge_animation() {
   targets: obj,
   loading: '100%',
   round: 1,
-  duration:2000,
+  duration:1000,
   easing: 'linear',
   update: function() {
     var el = document.querySelector('#JSobject pre');
     el.innerHTML = JSON.stringify(obj);
   }
-}).add({
-  targets: '#JSobject',
-  opacity: [1,0],
-});
+  }).add({
+    targets: '#JSobject',
+    opacity: [1,0],
+  });
   return timeline;
 }
 function init(){
     charge_animation();
-    setTimeout(tiles_open, 2250);
+    setTimeout(tiles_open, 1250);
+    setTimeout(home_to_section_animation, 3000);
 
 }
 
+function home_to_section_animation(){
+  anime.remove('div.quadrant_1');
+  anime.remove('div.quadrant_2');
+  anime.remove('div.quadrant_3');
+  anime.remove('div.quadrant_4');
+  var timeline = anime.timeline();
+  timeline.add({
+    targets: 'div.quadrant_3',
+    translateX: 300,
+    duration: 700,
+    easing: 'easeOutQuad',
+    offset: 0,
+  }).add({
+    targets: 'div.quadrant_4',
+    translateX: 300,
+    duration: 700,
+    easing: 'linear',
+    offset: 0,
+  }).add({
+    targets: 'div.quadrant_1',
+    translateX: 300,
+    duration: 700,
+    easing: 'easeInQuad',
+    delay: 10,
+    offset: 0,
+  }).add({
+    targets: 'div.quadrant_2',
+    translateX: 300,
+    duration: 700,
+    easing: 'easeInBack',
+    delay: 10,
+    offset: 0,
+  });
+  return timeline;
+}
 
 
 //}
