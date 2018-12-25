5b3cbf3a45fc7d4bb5156d8de671c8098026d695
Michael Fortunato <Michaelfortunato@uchicago.edu>
2018-12-24 22:17:38 -0500

    Transition Complete

--- main.js
+++ main.js
@@ -76,20 +76,26 @@ function charge_animation() {
     targets: '#JSobject',
     opacity: [1,0],
   });
+
   return timeline;
 }
 function init(){
     charge_animation();
     setTimeout(tiles_open, 1250);
-    setTimeout(home_to_section_animation, 3000);
+    document.getElementsByClassName('quadrant_1')[0].onclick = function(){home_to_section_animation('quadrant_1')};
+    document.getElementsByClassName('quadrant_2')[0].onclick = function(){home_to_section_animation('quadrant_2')};
+    document.getElementsByClassName('quadrant_3')[0].onclick = function(){home_to_section_animation('quadrant_3')};
+    document.getElementsByClassName('quadrant_4')[0].onclick = function(){home_to_section_animation('quadrant_4')};
+//    setTimeout(home_to_section_animation, 3000);
 
 }
 
-function home_to_section_animation(){
+function home_to_section_animation(quadrant){
   anime.remove('div.quadrant_1');
   anime.remove('div.quadrant_2');
   anime.remove('div.quadrant_3');
   anime.remove('div.quadrant_4');
+  charge_animation();
   var timeline = anime.timeline();
   timeline.add({
     targets: 'div.quadrant_3',
@@ -118,10 +124,19 @@ function home_to_section_animation(){
     delay: 10,
     offset: 0,
   });
-  return timeline;
+
+  setTimeout(function() {load_page(quadrant_pages[quadrant])}, 1200);
 }
+var quadrant_pages = {
+  'quadrant_1' : 'test.html',
+  'quadrant_2' : 'test.html',
+  'quadrant_3' : 'test.html',
+  'quadrant_4' : 'test.html'
+};
 
-
+function load_page(url){
+  $(":root").load(url);
+}
 //}
 //function open_animation(){
 //    center_cards();
